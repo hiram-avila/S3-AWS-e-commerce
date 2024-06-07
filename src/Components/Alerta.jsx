bb47c04493d83d6111a9b4c68e68f10a8aaf9cee
Hiram <Hiramavilaa@gmail.com>
2024-06-07 11:17:55 -0600

    changes

--- src/Components/Alerta.jsx
+++ src/Components/Alerta.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Alerta = ({alerta}) => {
     return (
-        <div className={`${alerta.error ? 'from-white to-red-600 rounded-2xl' : 'from-sky-400 to-sky-600'} bg-gradient-to-br text-center p-3 rounded-2xl uppercase text-white font-bold text-sm my-10`}>
+        <div className={`${alerta.error ? 'from-white to-red-400 rounded-2xl' : 'from-sky-400 to-sky-600'} bg-gradient-to-br text-center p-3 rounded-2xl uppercase text-white font-bold text-sm my-10`}>
             {alerta.msg}
         </div>
     )
