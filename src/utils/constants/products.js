cf2ad6cd4b9eb21e16ad13d16661cda33d9f8efc
Hiram <Hiramavilaa@gmail.com>
2024-06-24 20:41:56 -0600

    adding const products

---
+++ src/utils/constants/products.js
@@ -0,0 +1,11 @@
+const products = [
+    {
+      title: "Producto 1",
+      price: 19.99,
+      category: "Categoría A",
+      imagePath: "/assets/products/airpods.webp"
+    }
+  ];
+  
+  export default products;
+  
