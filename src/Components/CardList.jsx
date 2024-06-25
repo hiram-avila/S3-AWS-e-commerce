dc868638694328a3b82e0adeffb78efb0487daae
Hiram <Hiramavilaa@gmail.com>
2024-06-24 21:10:29 -0600

    fix issues

--- src/Components/CardList.jsx
+++ src/Components/CardList.jsx
@@ -2,7 +2,7 @@ import {useEffect} from 'react'
 import { useContext } from 'react';
 import { HashLoader } from "react-spinners";
 
-
+import products from '../utils/constants/products';
 
 import ProductsContext from '../context/ProductsProvider';
 import Card from './Card';
@@ -12,7 +12,7 @@ import Cargando from './Cargando';
 
 const CardList = () => {
 
-  const {productos} = useContext(ProductsContext)
+  const productos = products;
 
   return (
     <div className=" absolute bg-white grid grid-cols-4 gap-3 p-20 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 w-full overflow-hidden">
