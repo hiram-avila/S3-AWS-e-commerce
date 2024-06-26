029d0b884e6ac0c1a1e9e3fd6ecd7b2385dc513b
Hiram <Hiramavilaa@gmail.com>
2024-06-26 11:44:50 -0600

    new changes

--- src/Components/CardList.jsx
+++ src/Components/CardList.jsx
@@ -1,4 +1,4 @@
-import {useEffect} from 'react'
+import { useEffect } from 'react'
 import { useContext } from 'react';
 import { HashLoader } from "react-spinners";
 
@@ -12,20 +12,20 @@ import Cargando from './Cargando';
 
 const CardList = () => {
 
-  const {productos} = useContext(ProductsContext)
+  const { productos } = useContext(ProductsContext)
 
   console.log(productos)
 
   return (
     <div className=" absolute bg-white grid grid-cols-4 gap-3 p-20 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 w-full overflow-hidden">
-           <div>
-  {productos.map(product => (
-    <Card key={product.id} product={product} />
-  ))}
-</div>
-  
+      <div>
+        {productos.map(product => (
+          <Card key={product.id} product={product} />
+        ))}
+      </div>
+
     </div>
-    
+
   );
 };
 
