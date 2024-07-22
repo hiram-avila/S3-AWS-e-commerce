298ca78fc17cdb083a38825dbe07cbf7cbba2a99
Hiram <Hiramavilaa@gmail.com>
2024-07-21 20:15:01 -0600

    add image 1

--- src/Components/CardList.jsx
+++ src/Components/CardList.jsx
@@ -18,6 +18,7 @@ const CardList = () => {
 
   return (
     <div className="absolute bg-white grid grid-cols-4 gap-3 p-20 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 w-full overflow-hidden">
+      <img src="https://assetspwa.liverpool.com.mx/assets/digital/home/img/jul24/samsung_GB100724.jpg" alt="" />
       {productos.map(product => (
         <Card key={product.id} product={product} />
       ))}
