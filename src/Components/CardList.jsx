4fbe1d4e189f1fa96373fb24315fc6ef21373041
Hiram <Hiramavilaa@gmail.com>
2024-07-22 14:20:41 -0600

    fix accessibility performance 2

--- src/Components/CardList.jsx
+++ src/Components/CardList.jsx
@@ -10,11 +10,11 @@ const CardList = () => {
 
   return (
     <div className="relative">
-      {/* Header Image */}
-      <div className="relative w-full h-90 flex justify-center mt-2"> {/* Ajusta h-80 según sea necesario */}
+  
+      <div className="relative w-full h-90 flex justify-center mt-2"> 
         <img
           src={samsung}
-          alt="Header"
+          alt="phone"
           style={{
             width: '90%',
             height: '100%',
@@ -23,7 +23,7 @@ const CardList = () => {
         />
       </div>
 
-      {/* Product List */}
+     
       <div className="absolute top-0 left-0 bg-white grid grid-cols-4 gap-3 p-20 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 w-full overflow-hidden mt-80"> {/* Ajusta mt-80 según sea necesario */}
         {productos.map(product => (
           <Card key={product.id} product={product} />
