7afd6b8e7ceb29e47d642e401ba228ca06ece4b8
Hiram <Hiramavilaa@gmail.com>
2024-07-21 20:20:37 -0600

    add image header

--- src/Components/CardList.jsx
+++ src/Components/CardList.jsx
@@ -17,12 +17,18 @@ const CardList = () => {
   console.log(productos)
 
   return (
-    <div className="absolute bg-white grid grid-cols-4 gap-3 p-20 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 w-full overflow-hidden">
-      <img src="https://assetspwa.liverpool.com.mx/assets/digital/home/img/jul24/samsung_GB100724.jpg" alt="" />
+    <div className="relative">
+    <img 
+      src="https://assetspwa.liverpool.com.mx/assets/digital/home/img/jul24/samsung_GB100724.jpg" 
+      alt="Header"
+      className="w-full object-cover h-64" // Ajusta la altura (h-64) según tus necesidades
+    />
+    <div className="absolute top-0 left-0 bg-white grid grid-cols-4 gap-3 p-20 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 w-full overflow-hidden">
       {productos.map(product => (
         <Card key={product.id} product={product} />
       ))}
     </div>
+  </div>
 
   );
 };
