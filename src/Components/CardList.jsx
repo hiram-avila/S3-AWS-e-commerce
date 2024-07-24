0a5960532f20f9bf31ca6daa22cfcd131732ec2a
Hiram <Hiramavilaa@gmail.com>
2024-07-24 13:20:50 -0600

    test 8

---
+++ src/Components/CardList.jsx
@@ -0,0 +1,36 @@
+import { useContext } from 'react';
+import ProductsContext from '../context/ProductsProvider';
+import Card from './Card';
+import samsung from './../assets/products/samsung.webp';
+ 
+
+
+const CardList = () => {
+  const { productos } = useContext(ProductsContext);
+
+  return (
+    <div className="relative">
+  
+      <div className="relative w-full h-90 flex justify-center mt-2"> 
+        <img
+          src={samsung}
+          alt="phone"
+          style={{
+            width: '80%',
+            height: '100%',
+            objectFit: 'cover',
+          }}
+        />
+      </div>
+
+     
+      <div className="absolute top-0 left-0 bg-white grid grid-cols-4 gap-3 p-20 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 w-full overflow-hidden mt-80"> {/* Ajusta mt-80 según sea necesario */}
+        {productos.map(product => (
+          <Card key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CardList;
