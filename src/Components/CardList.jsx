9580dc22fb774fe5971578f410412c69bf31de75
Hiram <Hiramavilaa@gmail.com>
2024-07-24 14:40:42 -0600

    improving performance

--- src/Components/CardList.jsx
+++ src/Components/CardList.jsx
@@ -1,17 +1,22 @@
-import { useContext } from 'react';
+import { useEffect, useContext } from 'react';
 import ProductsContext from '../context/ProductsProvider';
 import Card from './Card';
 import samsung from './../assets/products/samsung.webp';
- 
-
 
 const CardList = () => {
   const { productos } = useContext(ProductsContext);
 
+  useEffect(() => {
+    const link = document.createElement('link');
+    link.rel = 'preload';
+    link.as = 'image';
+    link.href = samsung;
+    document.head.appendChild(link);
+  }, []);
+
   return (
     <div className="relative">
-  
-      <div className="relative w-full h-90 flex justify-center mt-2"> 
+      <div className="relative w-full h-90 flex justify-center mt-2">
         <img
           src={samsung}
           alt="phone"
@@ -23,8 +28,7 @@ const CardList = () => {
         />
       </div>
 
-     
-      <div className="absolute top-0 left-0 bg-white grid grid-cols-4 gap-3 p-20 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 w-full overflow-hidden mt-80"> {/* Ajusta mt-80 según sea necesario */}
+      <div className="absolute top-0 left-0 bg-white grid grid-cols-4 gap-3 p-20 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 w-full overflow-hidden mt-80">
         {productos.map(product => (
           <Card key={product.id} product={product} />
         ))}
