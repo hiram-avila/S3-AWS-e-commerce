af0fcc39c004be61855df3b3a190aebd066f44b7
Hiram <Hiramavilaa@gmail.com>
2024-07-25 10:46:37 -0600

    removing caret css

--- src/Components/CardList.jsx
+++ src/Components/CardList.jsx
@@ -15,7 +15,7 @@ const CardList = () => {
   }, []);
 
   return (
-    <div className="relative">
+    <div className="relative no-caret">
       <div className="relative w-full h-90 flex justify-center mt-2">
         <img
           src={samsung}
@@ -28,7 +28,7 @@ const CardList = () => {
         />
       </div>
 
-      <div className="absolute top-0 left-0 bg-white grid grid-cols-4 gap-3 p-20 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 w-full overflow-hidden mt-80">
+      <div className="absolute top-0 left-0 no-caret bg-white grid grid-cols-4 gap-3 p-20 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 w-full overflow-hidden mt-80">
         {productos.map(product => (
           <Card key={product.id} product={product} />
         ))}
